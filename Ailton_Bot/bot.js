f17facc87fe4d88df4d2f1fe1d796c9b3a1e9ab2
Flitz <62397386+Flitzs@users.noreply.github.com>
2021-09-30 20:37:23 -0300

    Update bot.js

--- Ailton_Bot/bot.js
+++ Ailton_Bot/bot.js
@@ -1,15 +1,23 @@
-console.log('Beep Bop')
+console.log('Beep Bop');
+
+const path = require('path');
+require('dotenv').config({path:'.env'});
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '!';
 const msg_e = require('discord.js');
-client.login();
+client.login(process.env.TOKEN);    
 
 client.on('ready', readyDiscord)
 
 function readyDiscord(msg){
     console.log("It's ALIVE!!!");
+    // console.log(process.env.TOKEN);
 }
 
 const commandHandler = require("./commands")
+
+//The commands are not working, it's not reading msg, 
+//watch the coding train tutorial again
+//after debuging post it
