f42412187f230fb7b079a7f89ac0d6d0583aeea3
Flitz <62397386+Flitzs@users.noreply.github.com>
2021-09-30 06:50:18 -0300

    Update bot.js

--- Ailton_Bot/bot.js
+++ Ailton_Bot/bot.js
@@ -13,18 +13,3 @@ function readyDiscord(msg){
 }
 
 const commandHandler = require("./commands")
-
-client.on('message', commandHandler, Discord); {
-	const newEmbed = new Discord.MessageEmbed()
-	.setColor(204, 255, 25)
-	.setTitle('Rules')
-	newEmbed.description
-	newEmbed.fields; (
-		{name: 'Rule 1', value: 'be noice'},
-		{name: 'Rule 2', value: "don't be not noice"},
-		{name: 'Rule 3', value: 'die'}
-	)
-	newEmbed.setThumbnail('/img/AIlton Bot pic.jfif')
-}
-
-
